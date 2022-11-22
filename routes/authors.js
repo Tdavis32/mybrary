aa5bc4d0576f20b73cec6cf33a5245b7626ae14d
Trent <davis23005@lcsd2kids.org>
2022-11-22 17:35:47 +0000

    functional second commit

--- routes/authors.js
+++ routes/authors.js
@@ -3,9 +3,13 @@ const router = express.Router()
 const Author = require('../models/author')
 //all authors
 router.get('/', async(req, res) => {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== "") {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
     try{
-        const authors = await Author.find({})
-        res.render('authors/index')
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {authors:authors, searchOptions:req.query})
     } catch {
         res.redirect("/")
     }
